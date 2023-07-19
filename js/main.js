fb2d1501969ebc6e124aacd5e8898bff6372f50b
Jeff Brayan Ajquejay Sitan <brayansitan99@gmail.com>
2023-07-18 21:50:26 -0600

    Mi Portafolio Personal

---
+++ js/main.js
@@ -0,0 +1,102 @@
+window.addEventListener('scroll',function(){
+    let animacion = document.getElementById('animado');
+    let posicion1 = animacion.getBoundingClientRect().top;
+    console.log(posicion1);
+    let tamañopantalla = window.innerHeight/1.3;
+
+    if(posicion1 < tamañopantalla){
+        animacion.style.animation = 'mover 1.4s ease-out'
+    }
+})
+
+window.addEventListener('scroll',function(){
+    let animacion = document.getElementById('animado2');
+    let posicion1 = animacion.getBoundingClientRect().top;
+    console.log(posicion1);
+    let tamañopantalla = window.innerHeight/1.3;
+
+    if(posicion1 < tamañopantalla){
+        animacion.style.animation = 'mover 1.4s ease-out'
+    }
+})
+
+window.addEventListener('scroll',function(){
+    let animacion = document.getElementById('animado3');
+    let posicion1 = animacion.getBoundingClientRect().top;
+    console.log(posicion1);
+    let tamañopantalla = window.innerHeight/1.3;
+
+    if(posicion1 < tamañopantalla){
+        animacion.style.animation = 'mover 1.4s ease-out'
+    }
+})
+window.addEventListener('scroll',function(){
+    let animacion = document.getElementById('animado4');
+    let posicion1 = animacion.getBoundingClientRect().top;
+    console.log(posicion1);
+    let tamañopantalla = window.innerHeight/1.2;
+
+    if(posicion1 < tamañopantalla){
+        animacion.style.animation = 'mover 1s ease-out'
+    }
+})
+window.addEventListener('scroll',function(){
+    let animacion = document.getElementById('animado5');
+    let posicion1 = animacion.getBoundingClientRect().top;
+    console.log(posicion1);
+    let tamañopantalla = window.innerHeight/1;
+
+    if(posicion1 < tamañopantalla){
+        animacion.style.animation = 'mover 1.4s ease-out'
+    }
+})
+window.addEventListener('scroll',function(){
+    let animacion = document.getElementById('animado6');
+    let posicion1 = animacion.getBoundingClientRect().top;
+    console.log(posicion1);
+    let tamañopantalla = window.innerHeight/1;
+
+    if(posicion1 < tamañopantalla){
+        animacion.style.animation = 'mover 1.4s ease-out'
+    }
+})
+window.addEventListener('scroll',function(){
+    let animacion = document.getElementById('animado7');
+    let posicion1 = animacion.getBoundingClientRect().top;
+    console.log(posicion1);
+    let tamañopantalla = window.innerHeight/1.4;
+
+    if(posicion1 < tamañopantalla){
+        animacion.style.animation = 'mover 1.4s ease-out'
+    }
+})
+window.addEventListener('scroll',function(){
+    let animacion = document.getElementById('animado8');
+    let posicion1 = animacion.getBoundingClientRect().top;
+    console.log(posicion1);
+    let tamañopantalla = window.innerHeight/1.3;
+
+    if(posicion1 < tamañopantalla){
+        animacion.style.animation = 'mover 1.4s ease-out'
+    }
+})
+window.addEventListener('scroll',function(){
+    let animacion = document.getElementById('tecb');
+    let posicion1 = animacion.getBoundingClientRect().top;
+    console.log(posicion1);
+    let tamañopantalla = window.innerHeight/1.3;
+
+    if(posicion1 < tamañopantalla){
+        animacion.style.animation = 'mover 1.4s ease-out'
+    }
+})
+window.addEventListener('scroll',function(){
+    let animacion = document.getElementById('cert');
+    let posicion1 = animacion.getBoundingClientRect().top;
+    console.log(posicion1);
+    let tamañopantalla = window.innerHeight/1.3;
+
+    if(posicion1 < tamañopantalla){
+        animacion.style.animation = 'mover 1.4s ease-out'
+    }
+})
